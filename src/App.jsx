6ba83ff1e5ab09758eb0348f44b09637cc02e961
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-08-25 16:01:21 +0700

    fix: router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import Earthmoon from './components/Templates/Earthmoon.jsx';
-import BigDataClub from './components/Templates/BigDataClub.jsx';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
