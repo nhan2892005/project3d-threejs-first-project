025d86404011e201fc26bd097ee27c3569f2efa3
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-08-25 15:59:38 +0700

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import Earthmoon from './components/Templates/Earthmoon.jsx';
 import BigDataClub from './components/Templates/BigDataClub.jsx';
@@ -8,12 +8,12 @@ const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
           <Route path="/" exact element={<Home />} /> 
           <Route path="/earth-and-moon" exact element={<Earthmoon />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
